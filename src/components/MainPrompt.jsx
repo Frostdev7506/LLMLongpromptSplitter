49cbefcf943894fe9078b86aa208ffedd29bace0
Neeraj Butola <Neerajbutola234@gmail.com>
2023-10-15 22:45:37 +0530

    Added text area and other buttons

--- src/components/MainPrompt.jsx
+++ src/components/MainPrompt.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/MainPrompt.css";
 
 const MainPrompt = () => {
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const handleSelectChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
   return (
     <>
       <div className="main-prompt">
@@ -15,6 +21,25 @@ const MainPrompt = () => {
           cols="50"
           rows="20"
         ></textarea>
+        <label className="llm-type" htmlFor="llm-type">
+          Choose LLM Type:
+        </label>
+        <select name="llm-type" id="llm-type" onChange={handleSelectChange}>
+          <option value="">--Please choose an option--</option>
+          <option value="openai">Chat GPT 3.5</option>
+          <option value="bing">Bing Chat GPT 4</option>
+          <option value="bard">Bard</option>
+          <option value="custom">Custom</option>
+        </select>
+        {selectedOption === "custom" && (
+          <>
+            <label className="custom-input-label" htmlFor="custom-input">
+              Custom Input:
+            </label>
+            <input className="custom-input" type="text" id="custom-input" />
+          </>
+        )}
+        <button type="submit">Split</button>
       </div>
     </>
   );
